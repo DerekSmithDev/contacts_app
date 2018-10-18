eb440c9e27adae1e6f5088d86a6cb32a562158b0
dereksmith83 <mdsmith1983@gmail.com>
2018-10-17 21:33:46 -0500

    added middle names, bios, and changed full name method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,6 @@ class Contact < ApplicationRecord
     updated_at.strftime("%m/%d/%y")
   end
   def full_name
-    last_name + ", " + first_name
+    last_name + ", " + first_name + " " + middle_name
   end
 end
