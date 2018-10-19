652354eedf6c68a789423613faad6faeddf6e6ac
dereksmith83 <mdsmith1983@gmail.com>
2018-10-18 21:42:41 -0500

    fix syntax error

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -3,7 +3,7 @@ class Api::ContactsController < ApplicationController
   def index
     @contacts = Contact.all
     first_name = params[:first_name]
-    if search_name
+    if first_name
       @contacts = @contacts.where("first_name ILIKE ?", "#{first_name}")
     end
     render "index.json.jbuilder"
